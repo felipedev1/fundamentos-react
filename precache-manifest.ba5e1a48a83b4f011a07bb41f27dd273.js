d841a3e3664f558dde645a686b6ead13a9ff336b
Felipe <felipe.pereira.dev@gmail.com>
2020-05-18 10:59:34 -0300

    Updates

---
+++ precache-manifest.ba5e1a48a83b4f011a07bb41f27dd273.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2adf5ee7929b522146020e6732d8358f",
+    "url": "/fundamentos-react/index.html"
+  },
+  {
+    "revision": "3f9fe4c798b30dd9aee1",
+    "url": "/fundamentos-react/static/css/main.520e9d71.chunk.css"
+  },
+  {
+    "revision": "1accff1b28869003646e",
+    "url": "/fundamentos-react/static/js/2.c201a2a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fundamentos-react/static/js/2.c201a2a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f9fe4c798b30dd9aee1",
+    "url": "/fundamentos-react/static/js/main.3d2faea3.chunk.js"
+  },
+  {
+    "revision": "539caaa77bb6af3ed549",
+    "url": "/fundamentos-react/static/js/runtime-main.0b30a611.js"
+  }
+]);
